a160e77a1ae2bac27372f3b4f6ac90837d87848a
Junior <nilson.josende@fatec.sp.gov.br>
2025-10-25 02:49:34 -0300

    commit inicial

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php
+$host = "mysql:host=localhost;dbname=projeto_estoque";
+$user = "root";
+$pass = "8rj0cAWniS9sVWpH#";
+phpinfo();
+try {
+    $pdo = new PDO($host, $user, $pass);
+} catch (Exception $e) {
+    die("Error:".$e->getMessage());
+}
+?>
