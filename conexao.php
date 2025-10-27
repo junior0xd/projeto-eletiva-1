7e4ee56709198aa2882803741f8c3fdf08729a14
Junior <nilson.josende@fatec.sp.gov.br>
2025-10-26 21:17:05 -0300

    updt

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 $host = "mysql:host=localhost;dbname=projeto_estoque";
 $user = "root";
 $pass = "8rj0cAWniS9sVWpH#";
-phpinfo();
+
 try {
     $pdo = new PDO($host, $user, $pass);
 } catch (Exception $e) {
