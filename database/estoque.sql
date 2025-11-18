1fe0dbf748d73a764842502064ea72f7104370bc
Junior <149541055+junior0xd@users.noreply.github.com>
2025-11-18 10:47:46 -0300

    adicionado categoria produto e relacionamento entre tabelas

--- database/estoque.sql
+++ database/estoque.sql
@@ -9,6 +9,11 @@ CREATE TABLE IF NOT EXISTS usuario(
     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS categoria(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 CREATE TABLE IF NOT EXISTS produto(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
@@ -16,4 +21,11 @@ CREATE TABLE IF NOT EXISTS produto(
     quantidade INT NOT NULL,
     data_validade DATE,
     data_adicionado TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    categoria INT NOT NULL,
+    INDEX fk_produto_categoria (categoria_id ASC),
+    CONSTRAINT fk_produto_categoria
+        FOREIGN KEY (categoria)
+        REFERENCES categoria.id
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
