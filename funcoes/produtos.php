0a7d37d2b0edd72aa19dbb891ac9a3d8906c11da
Junior <nilson.josende@fatec.sp.gov.br>
2025-11-21 19:04:39 -0300

    funcao para adicionar novos produtos

--- funcoes/produtos.php
+++ funcoes/produtos.php
@@ -1,12 +1,30 @@
 <?php
 class Produto
 {
-    private PDO $pdo; 
-
-    public function __construct(PDO $db) {
-        $this->pdo = $db;
-    }
+    public function __construct(protected PDO $pdo) {}
     
+    public function adicionar_produto($nome, $quantidade, $categoria) {
+        try {
+        $stmt_checar = $this->pdo->prepare("SELECT COUNT(*) FROM produto WHERE nome = :nome");
+        $stmt_checar->bindParam(":nome", $nome, PDO::PARAM_STR);
+        $stmt_checar->execute();
+        $contador = $stmt_checar->fetchColumn();
+        if ($contador > 0) {
+            //produto já existe
+            return 3;
+        } else {
+            $stmt = $this->pdo->prepare("INSERT INTO produto (nome, quantidade, categoria_id) VALUES (?, ?, ?)");
+            if ($stmt->execute([$nome, $quantidade, $categoria])) {
+                //produto adicionado com sucesso
+                return 1;
+            }
+            return 2;
+        }
+    } catch (Exception $e) {
+        echo ("Ocorreu um erro ao adicionar um novo produto: " . $e->getMessage());
+        return 2;
+    }
+    }
     public function recuperar_produtos()
     {
         try {
@@ -17,7 +35,6 @@ class Produto
         }
         return $produtos;
     }
-
     public function recuperar_categorias()
     {
         try {
