5c2190626f36144cbebe693b7c2f5614bc5353cf
Junior <nilson.josende@fatec.sp.gov.br>
2025-11-21 15:26:30 -0300

    consertado foreign key

--- database/estoque.sql
+++ database/estoque.sql
@@ -6,12 +6,13 @@ CREATE TABLE IF NOT EXISTS usuario(
     nome VARCHAR(100) NOT NULL,
     cadastro VARCHAR(50) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
+    cargo TINYINT,
     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS categoria(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
+    nome VARCHAR(255) NOT NULL
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS produto(
@@ -20,12 +21,12 @@ CREATE TABLE IF NOT EXISTS produto(
     descricao TEXT,
     quantidade INT NOT NULL,
     data_validade DATE,
-    data_adicionado TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    categoria INT NOT NULL,
-    INDEX fk_produto_categoria (categoria_id ASC),
+    data_adicionado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    categoria_id INT NOT NULL,
+    INDEX fk_produto_categoria_id (categoria_id ASC),
     CONSTRAINT fk_produto_categoria
-        FOREIGN KEY (categoria)
-        REFERENCES categoria.id
+        FOREIGN KEY (categoria_id)
+        REFERENCES projeto_estoque.categoria (id)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
