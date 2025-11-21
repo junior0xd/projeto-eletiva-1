f3cf27aac1e6e3fd7b5b9b49f70f9fe0ba5c2c36
Junior <nilson.josende@fatec.sp.gov.br>
2025-11-21 18:02:04 -0300

    adicionado parametro opcional

--- funcoes/echo-out.php
+++ funcoes/echo-out.php
@@ -1,15 +1,15 @@
-<?php function echoSucesso($mensagem){ //ideia: adicionar em uma classe e passar as classes desejadas como parametros também ?>
-    <div class="alert alert-success d-flex align-items-center mb-3" role="alert">
+<?php function echoSucesso($mensagem, $classes = "alert alert-success d-flex align-items-center mb-3"){ ?>
+    <div class="<?=$classes?>" role="alert">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
         </svg>
         <div class="ms-4">
-            <?= $mensagem ?>
+            <?= $mensagem || $classes ?>
         </div>
     </div>
 <?php } ?>
-<?php function echoAlertaWarning($mensagem){ ?>
-    <div class="alert alert-warning d-flex align-items-center mb-3" role="alert">
+<?php function echoAlertaWarning($mensagem, $classes = "alert alert-warning d-flex align-items-center mb-3"){ ?>
+    <div class="<?=$classes?>" role="alert">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
         </svg>
@@ -18,9 +18,9 @@
         </div>
     </div>
 <?php } ?>
-<?php function echoAlertaDanger($mensagem){  ?>
+<?php function echoAlertaDanger($mensagem, $classes = "alert alert-danger d-flex align-items-center mb-3"){  ?>
 
-    <div class="alert alert-danger d-flex align-items-center mb-3" role="alert">
+    <div class="<?=$classes?>" role="alert">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
         </svg>
