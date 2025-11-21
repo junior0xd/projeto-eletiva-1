b7ef8c92af4af4d456f8283e72e3fb180fd19fad
Junior <nilson.josende@fatec.sp.gov.br>
2025-11-21 18:05:25 -0300

    renomeado

--- funcoes/recuperar_produtos.php
+++
@@ -1,8 +0,0 @@
-<?php 
-try {
-    $stmt = $pdo->query("SELECT * FROM produto");
-    $produtos = $stmt->fetchAll();
-} catch (Exception $e) {
-    echo "Erro ao recuperar produtos: " . $e->getMessage();
-}
-?>
