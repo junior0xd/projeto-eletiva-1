3b4724b4cc0e328153adb178b2fac46258ab023f
Junior <nilson.josende@fatec.sp.gov.br>
2025-11-23 00:43:18 -0300

    adicionado modal editar

---
+++ pages/modal_editar_produto.php
@@ -0,0 +1,15 @@
+<div class="modal fade" id="editarProduto" tabindex="-1" aria-labelledby="editarProdutoLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editarNomeProduto">Placeholder</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Into the dark</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
