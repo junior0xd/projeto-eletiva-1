13343c842a83d0ae71d78f087f11dd86990fa78f
Junior <nilson.josende@fatec.sp.gov.br>
2025-11-26 20:12:40 -0300

    adicionado variaveis de ambiente

---
+++ envs.php
@@ -0,0 +1,7 @@
+<?php
+// Don't mind it, i will put out of public html folder in production.
+putenv('DB_HOST=mysql:host=localhost;dbname=projeto_estoque');
+putenv('DB_USER=code');
+putenv('DB_PASS=vscode123');
+putenv('ERROR_LOG_PATH=/home/bisel/Documentos/projeto-eletiva-1/php_errors.log');
+?>
