1a70be39c1185703e83cde6d5d019fde4e3bf3bc
Junior <nilson.josende@fatec.sp.gov.br>
2025-11-26 18:58:14 -0300

    adicionado, desenvolver depois

---
+++ pages/adicionar_produtos.php
@@ -0,0 +1,11 @@
+<?php
+require('../funcoes/sessao.php');
+require('../funcoes/auth.php');
+Auth::verificar_sessao_ativa();
+define('IN_APP', true);
+require('head-navbar.php'); 
+?>
+<main>
+
+</main>
+<?php require('footer.php'); ?>
