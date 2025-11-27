fc0b3acffb551b839fcb7529c7f72a3cfcd52656
Junior <nilson.josende@fatec.sp.gov.br>
2025-11-26 22:52:28 -0300

    atualizado politica de seguranca

--- funcoes/security-headers.php
+++ funcoes/security-headers.php
@@ -2,7 +2,7 @@
 header("X-Frame-Options: DENY");
 header("X-Content-Type-Options: nosniff");
 header("X-XSS-Protection: 1; mode=block");
-header("Content-Security-Policy: default-src 'self'");
+header("Content-Security-Policy: default-src 'self'; img-src 'self' data: http://www.w3.org; script-src 'self'; style-src 'self' 'unsafe-inline';");
 ini_set('session.cookie_httponly', 1);
 ini_set('session.cookie_samesite', 'Strict');
 ?>
