42836ebf42c048f91288779cc841725deea72fdb
Junior <nilson.josende@fatec.sp.gov.br>
2025-11-27 18:58:08 -0300

    corrigido icone lixeira

--- assets/registrar_estoque.js
+++ assets/registrar_estoque.js
@@ -1,3 +1,4 @@
+   var iconeLixeira = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'><path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0'/></svg>";
    function matchCustom(params, data) {
     // If there are no search terms, return all of the data
     if ($.trim(params.term) === '') {
@@ -52,7 +53,7 @@ $(document).ready(function() {
             // input quantidade
             container.append($('<input>', { type: 'number', name: 'quantidades[]', class: 'form-control', min: 0, value: 0, style: 'width:7.5rem' }));
             // botão remover
-            container.append($('<button>', { type: 'button', class: 'btn btn-sm btn-secondary btn-remover' }).html("<?= iconeLixeiraJquery() ?>"));
+            container.append($('<button>', { type: 'button', class: 'btn btn-sm btn-secondary btn-remover' }).html(iconeLixeira));
             $('#selecionadosContainer').append(container);
         }
     });
@@ -110,7 +111,7 @@ $(document).ready(function() {
             // input quantidade
             container.append($('<input>', { type: 'number', name: 'quantidades[]', class: 'form-control', min: 0, value: 0, style: 'width:7.5rem' }));
             // botão remover
-            container.append($('<button>', { type: 'button', class: 'btn btn-sm btn-secondary btn-remover' }).html("<?= iconeLixeiraJquery() ?>"));
+            container.append($('<button>', { type: 'button', class: 'btn btn-sm btn-secondary btn-remover' }).html(iconeLixeira));
             $('#selecionadosContainerSaida').append(container);
         }
     });
