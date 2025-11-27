c277d255673516341f8298fc0b7403e4cbcafa91
Junior <nilson.josende@fatec.sp.gov.br>
2025-11-26 22:52:46 -0300

    movido logica para assets/estoque.js

--- pages/estoque.php
+++ pages/estoque.php
@@ -206,27 +206,4 @@ require('head-navbar.php');
     <?php include('modal_detalhe_produto.php'); ?>
 </main>
 <?php require('footer.php'); ?>
-<script>
-document.getElementById('detalheProduto').addEventListener('show.bs.modal', (e) => {
-    const produto = JSON.parse(e.relatedTarget.getAttribute('detalhe-produto'));
-    if (produto.categoria_id === 1){
-        produto.categoria = 'Enfermagem';
-    } else if (produto.categoria_id === 2){
-        produto.categoria = 'Escritório';
-    } else {
-        produto.categoria = 'Desconhecida';
-    }
-    document.getElementById('detalheNomeProduto').textContent = produto.nome;
-    document.getElementById('detalheQuantidade').textContent = produto.quantidade;
-    document.getElementById('detalheValidade').textContent = produto.data_validade;
-    document.getElementById('detalheCategoria').textContent = produto.categoria;
-    });
-document.getElementById('editarProduto').addEventListener('show.bs.modal', (e) => {
-    const produto = JSON.parse(e.relatedTarget.getAttribute('detalhe-produto'));
-    document.getElementById('editarNomeProduto').textContent = produto.nome;
-    document.getElementById('editarProdutoId').value = produto.id;
-    document.getElementById('editarNomeProduto2').value = produto.nome;
-    document.getElementById('editarValidadeProduto').value = produto.data_validade_iso;
-    document.getElementById('editarQuantidadeProduto').value = produto.quantidade;
-    });
-</script>
+<script src="../assets/estoque.js"></script>
