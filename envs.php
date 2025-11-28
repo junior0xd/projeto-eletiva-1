b77a764cdbef317ca115298353b1bfb645fdb5b8
Junior <nilson.josende@fatec.sp.gov.br>
2025-11-27 22:02:26 -0300

    env quantidade minima

--- envs.php
+++ envs.php
@@ -4,4 +4,5 @@ putenv('DB_HOST=mysql:host=localhost;dbname=projeto_estoque');
 putenv('DB_USER=code');
 putenv('DB_PASS=vscode123');
 putenv('ERROR_LOG_PATH=/home/bisel/Documentos/projeto-eletiva-1/php_errors.log');
+putenv('QUANTITY_MIN_ALERT=5');
 ?>
