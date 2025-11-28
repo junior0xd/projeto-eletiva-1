2368e31d1ae61fb1867199e51656350d1bbf4aed
Junior <nilson.josende@fatec.sp.gov.br>
2025-11-27 22:29:01 -0300

    novas variaveis

--- envs.php
+++ envs.php
@@ -5,4 +5,7 @@ putenv('DB_USER=code');
 putenv('DB_PASS=vscode123');
 putenv('ERROR_LOG_PATH=/home/bisel/Documentos/projeto-eletiva-1/php_errors.log');
 putenv('QUANTITY_MIN_ALERT=5');
+putenv('PASSWORD_LENGTH_MIN=8');
+putenv('ROLE_ADMIN=60');
+putenv('ROLE_USER=32');
 ?>
