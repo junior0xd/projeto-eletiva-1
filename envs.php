76aef4db006bfd060a85251271ad01221adef669
Junior <nilson.josende@fatec.sp.gov.br>
2025-12-01 21:51:59 -0300

    criado variavel de ambiente

--- envs.php
+++ envs.php
@@ -6,6 +6,7 @@ putenv('DB_PASS=vscode123');
 putenv('ERROR_LOG_PATH=/home/bisel/Documentos/projeto-eletiva-1/php_errors.log');
 putenv('QUANTITY_MIN_ALERT=5');
 putenv('PASSWORD_LENGTH_MIN=8');
+putenv('SESSION_TIMEOUT=1800'); // 30 minutos
 putenv('ROLE_ADMIN=60');
 putenv('ROLE_USER=32');
 putenv('RECORDS_PER_PAGE=8');
