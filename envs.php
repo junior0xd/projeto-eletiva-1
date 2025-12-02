a31a445807a00fa342219ae0bfe0bb9c2abb44e3
Junior <nilson.josende@fatec.sp.gov.br>
2025-12-01 22:34:22 -0300

    fixed reference

--- envs.php
+++ envs.php
@@ -3,7 +3,7 @@
 putenv('DB_HOST=mysql:host=localhost;dbname=projeto_estoque');
 putenv('DB_USER=code');
 putenv('DB_PASS=vscode123');
-putenv('ERROR_LOG_PATH=/home/bisel/Documentos/projeto-eletiva-1/php_errors.log');
+putenv('ERROR_LOG_PATH=../logs/php_errors.log');
 putenv('QUANTITY_MIN_ALERT=5');
 putenv('PASSWORD_LENGTH_MIN=8');
 putenv('SESSION_TIMEOUT=1800'); // 30 minutos
