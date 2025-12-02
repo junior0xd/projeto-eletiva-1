b81df1f7610b3f0a0e8f2a3be95f9f65c2c57f27
Junior <nilson.josende@fatec.sp.gov.br>
2025-12-01 21:52:11 -0300

    alterado a regra

--- funcoes/authorization.php
+++ funcoes/authorization.php
@@ -1,5 +1,5 @@
 <?php
-if(!isset($_SESSION['cargo']) && ($_SESSION['cargo'] !== getenv('ROLE_ADMIN') && $_SESSION['cargo'] !== getenv('ROLE_USER'))) {
+if($_SESSION['cargo'] !== getenv('ROLE_ADMIN') && $_SESSION['cargo'] !== getenv('ROLE_USER')) {
     include('forbidden.php');
     exit();
 } 
